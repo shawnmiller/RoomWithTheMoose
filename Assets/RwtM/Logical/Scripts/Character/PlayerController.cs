827f46abdef56dc766eaac836df627ab79ab170a
Shawn Miller <shawn.miller0@gmail.com>
2013-05-31 22:39:00 -0400

    Player Movement

--- Assets/RwtM/Logical/Scripts/Character/PlayerController.cs
+++ Assets/RwtM/Logical/Scripts/Character/PlayerController.cs
@@ -1,36 +1,95 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+  private bool controllable;
+
   [SerializeField]
   private float walkSpeed;
   [SerializeField]
   private float runSpeed;
   [SerializeField]
+  private float gravity;
+  [SerializeField]
   private float stamina;
+  [SerializeField]
+  private float staminaRegeneration;
+  [SerializeField]
+  private float rerunWaitTime;
+  private float stopRunTime;
   private float currentStamina;
-
   private bool isRunning;
-
-  // Cache GameState
+  
+  // Cached objects
   private GameState gameState;
+  private CharacterController controller;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+  {
     gameState = GameState.Get ();
+    controller = transform.GetComponent<CharacterController> ();
     currentStamina = stamina;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+  {
     if (!(gameState.State == StateType.In_Game))
     {
       return;
     }
 
+    float currentSpeed = walkSpeed;
+
+    // Stringy run handling code
+    // TODO: Clean up
+    if (Input.GetKey (KeyCode.LeftShift) && Time.time > stopRunTime + rerunWaitTime)
+    {
+      isRunning = true;
+      currentSpeed = runSpeed;
+    }
+    else
+    {
+      if (isRunning)
+      {
+        stopRunTime = Time.time;
+      }
+      isRunning = false;
+    }
+
     if (!isRunning)
     {
-
+      currentStamina = Mathf.Min (stamina, currentStamina + (staminaRegeneration * Time.fixedDeltaTime));
     }
+    else
+    {
+      currentStamina = Mathf.Max (0, currentStamina - Time.fixedDeltaTime);
+    }
+
+    Vector3 moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+    moveDirection = transform.TransformDirection (moveDirection);
+    moveDirection = moveDirection.normalized;
+    moveDirection = AlignToGroundNormal (moveDirection);
+    moveDirection *= currentSpeed * Time.fixedDeltaTime;
+
+    controller.Move (moveDirection);
 	}
+
+  private Vector3 AlignToGroundNormal (Vector3 direction)
+  {
+    RaycastHit hit;
+    if (Physics.Raycast (transform.position, Vector3.down, out hit))
+    {
+      Vector3 temp = Vector3.Cross (Vector3.up, direction);
+      temp = Vector3.Cross (temp, hit.normal);
+      return temp;
+    }
+    else
+    {
+      return direction;
+    }
+  }
 }
