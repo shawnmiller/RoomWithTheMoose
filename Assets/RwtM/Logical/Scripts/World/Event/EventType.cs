8ac244eba435e9ebff976fac5a898d45af16f00f
Shawn Miller <shawn.miller0@gmail.com>
2013-06-05 20:36:24 -0400

    EventType Added

---
+++ Assets/RwtM/Logical/Scripts/World/Event/EventType.cs
@@ -0,0 +1,14 @@
+﻿public enum EventType
+{
+  Spawn_Object,
+  Destroy_Object,
+  Move_Object_Instant,
+  Move_Object_Timed,
+  Rotate_Object_Instant,
+  Rotate_Object_Timed,
+  Lock_Player_Movement,
+  Lock_Player_Camera,
+  Play_Sound,
+  Play_Animation,
+  Unlock_Event
+}
