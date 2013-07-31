9305b43024f3d2d76d0a75fe2a07b750bce09316
Shawn Miller <shawn.miller0@gmail.com>
2013-07-31 19:37:08 -0400

    Core Extraction

---
+++ Assets/RwtM/Logical/Scripts/File Management/CoreExtraction.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class CoreExtraction : MonoBehaviour
+{
+  void Start()
+  {
+    if (Application.isEditor)
+    {
+      return;
+    }
+
+    string path = Application.dataPath + Globals.FILE_EXTRACTION_ROOT;
+    if(!Directory.Exists(path))
+    {
+      ExtractFiles();
+    }
+  }
+
+  void ExtractFiles()
+  {
+    /*******************************************************************************
+     * The majority--if not the entirety--of the files that we will be extracting
+     * are XML files. Because they are going to be grabbed as TextAssets from the
+     * Resources folder, we have no information on them at all outside of the
+     * text contained within them. We need a way to determine where these files
+     * are going to be extracted to. We also need to allow the developers to run
+     * the game without having to build every single time, so this requires that
+     * the information not impede on the file's content in any way. In order to
+     * meet both of these requirements, the first line of every file will contain
+     * this path and file name within a comment using the
+     * Globals.EXTRACTION_FILE_HEADER_OPEN and Globals.EXTRACTION_FILE_HEADER_CLOSE
+     * values.
+     ******************************************************************************/
+    TextAsset[] pull = Resources.LoadAll(Globals.EXTRACTION_RESOURCE_FOLDER) as TextAsset[];
+
+    foreach (TextAsset asset in pull)
+    {
+      string[] lines = asset.text.Split(new char[] {'\n'});
+
+      Regex cRemove = new Regex("[<>- ]");
+      string pathLine = cRemove.Replace(lines[0], "");
+      string fileName = pathLine.Substring(pathLine.LastIndexOf('/'), pathLine.Length - 1);
+
+      string directory = Application.dataPath + pathLine.Substring(0, pathLine.LastIndexOf('/'));
+      
+      if (!Directory.Exists(directory))
+      {
+        Directory.CreateDirectory(directory);
+      }
+    }
+  }
+}
--- Assets/RwtM/Logical/Scripts/Misc/Globals.cs
+++ Assets/RwtM/Logical/Scripts/Misc/Globals.cs
@@ -3,4 +3,9 @@
   public const int CAM_LOOK_LEFT = -1;
   public const int CAM_LOOK_NORMAL = 0;
   public const int CAM_LOOK_RIGHT = 1;
+
+  public const string EXTRACTION_RESOURCE_FOLDER = @"Extraction";
+  public const string FILE_EXTRACTION_ROOT = @"/data/";
+  public const string EXTRACTION_FILE_HEADER_OPEN = @"<!--";
+  public const string EXTRACTION_FILE_HEADER_CLOSE = @"-->";
 }
