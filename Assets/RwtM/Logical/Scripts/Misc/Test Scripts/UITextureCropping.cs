fb14dc742665fde7f18b4d9193cc67e7a3115c9a
Shawn Miller <shawn.miller0@gmail.com>
2013-08-02 01:45:21 -0400

    UI Texture Test

---
+++ Assets/RwtM/Logical/Scripts/Misc/Test Scripts/UITextureCropping.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UITextureCropping : MonoBehaviour
+{
+  public Texture2D texture;
+  public float percent = 1f;
+
+  void OnGUI()
+  {
+    Graphics.DrawTexture(new Rect(0, 0, Screen.width * percent, Screen.height), texture, new Rect(0, 0, percent, 1), 0, 0, 0, 0);
+    percent = GUILayout.HorizontalSlider(percent, 0f, 1f, GUILayout.Width(Screen.width), GUILayout.Height(10f));
+  }
+}
