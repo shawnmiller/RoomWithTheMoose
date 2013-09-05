1f0db96de2559e3687be1707d0d505069eb1a073
Shawn Miller <shawn.miller0@gmail.com>
2013-09-05 05:28:27 -0400

    Removed Extraneous Folder

---
+++ Assets/RwtM/Scripts/File Management/PathMaker.cs
@@ -0,0 +1,29 @@
+public static class PathMaker
+{
+  ///<summary>
+  /// Takes any amount of strings and converts them to a file path.
+  /// Prereq: All separators have been removed from each string in pathPieces.
+  ///</summary>
+  ///<param name="separator"> The character used for separating each folder/file. </param>
+  ///<param name="pathPieces"> A string array with a sorted path from root -> filename.ext </param>
+  public static string BuildPath(string[] pathPieces, string separator)
+  {
+    if(pathPieces.Length == 1)
+    {
+      return pathPieces[0];
+    }
+    
+    string results = "";
+    
+    for(int i=0; i<pathPieces.Length; ++i)
+    {
+      results += pathPieces[i];
+      if(i != pathPieces.Length - 1)
+      {
+        results += separator;
+      }
+    }
+    
+    return results;
+  }
+}
