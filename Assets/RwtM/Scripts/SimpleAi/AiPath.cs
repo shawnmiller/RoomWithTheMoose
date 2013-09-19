580eadd09b6f7569897c22a6fc88b3f1d6672d7d
Shawn <shawn@shawn-AOD270.(none)>
2013-09-18 22:16:24 -0400

    Minor Addition

--- Assets/RwtM/Scripts/SimpleAi/AiPath.cs
+++ Assets/RwtM/Scripts/SimpleAi/AiPath.cs
@@ -17,4 +17,28 @@ public class AiPath : GameComponent
       return;
     }
   }
+
+  public void SetOnPath(GameObject ai, int pathNode)
+  {
+    if(pathNode >= path.Length)
+    {
+      Debug.LogError("Attempted to place Ai " + ai.name + " on path " + transform.root.gameObject.name + 
+                     " but the pathNode exceeds the length of the path.");
+      return;
+    }
+
+    ai.transform.position = path[pathNode].transform.position;
+    ai.transform.rotation = path[pathNode].transform.rotation;
+  }
+
+  public void Move(GameObject actor)
+  {
+    if(state != StateType.In_Game)
+    {
+      return;
+    }
+
+    SimpleAi ai = actor.GetComponent<SimpleAi>();
+    
+  }
 }
---
+++ Assets/RwtM/Scripts/SimpleAi/SimpleAi.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SimpleAi : GameComponent
+{
+  public AiPath myPath;
+  public int pathDirection = 1;
+}
