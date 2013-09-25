d0058c4c723e6545ef60919a79fff5ab2992ee2e
Shawn Miller <shawn.miller0@gmail.com>
2013-09-25 03:09:22 -0400

    added custom timekeeper class

---
+++ Assets/RwtM/Scripts/Required/TimeKeeper.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TimeKeeper
+{
+  public enum KeeperMode
+  {
+    FixedUpdate,
+    Update,
+    Custom
+  }
+
+  public KeeperMode mode;
+
+  private float timer;
+
+  public TimeKeeper(KeeperMode mode)
+  {
+    this.mode = mode;
+  }
+
+  public void Tick()
+  {
+    if (mode == KeeperMode.Custom)
+    {
+      Debug.LogError("TimeKeeper is recording a custom timer, but a 0 parameter Tick() function is being called.");
+      return;
+    }
+
+    if (mode == KeeperMode.FixedUpdate)
+    {
+      timer += Time.fixedDeltaTime;
+    }
+    else
+    {
+      timer += Time.deltaTime;
+    }
+  }
+
+  public void Tick(float time)
+  {
+    if (mode != KeeperMode.Custom)
+    {
+      Debug.LogWarning("TimeKeeper was called using Tick(float) but is recording a specific time.");
+    }
+    timer += time;
+  }
+
+  public float GetTime()
+  {
+    return timer;
+  }
+}
