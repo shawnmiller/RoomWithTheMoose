63d41a2046c22e451bc58140442be85a52aa9261
Shawn Miller <shawn.miller0@gmail.com>
2013-09-26 05:10:34 -0400

    added world state tracking and texture changes

---
+++ Assets/RwtM/Scripts/World/Transitioning/WorldState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WorldState : Singleton<WorldState>
+{
+  public float position = 0f;
+
+  void FixedUpdate()
+  {
+    Clamp();
+  }
+
+  void Clamp()
+  {
+    if (position < 0f)
+    {
+      position = 0f;
+      return;
+    }
+    if (position > 1f)
+    {
+      position = 1f;
+    }
+  }
+}
