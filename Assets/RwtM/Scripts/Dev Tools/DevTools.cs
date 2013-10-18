6dc6a923126a325b34804dc1d26ed280030f40cc
Shawn Miller <shawn.miller0@gmail.com>
2013-10-18 13:37:32 -0400

    updated proof of concept 2
    dev tools are now in the project

---
+++ Assets/RwtM/Scripts/Dev Tools/DevTools.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+  //public bool sensitivityWindow;
+  //CameraController camControl;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.U))
+    {
+      Screen.showCursor = !Screen.showCursor;
+      Screen.lockCursor = !Screen.lockCursor;
+    }
+    if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.R))
+    {
+      Application.LoadLevel(0);
+    }
+
+    /*if (Input.GetKeyDown(KeyCode.T))
+    {
+      sensitivityWindow = !sensitivityWindow;
+    }*/
+  }
+
+  /*void OnGUI()
+  {
+    if (sensitivityWindow)
+    {
+      Debug.Log("Drawing Window");
+      GUI.Window(Constants.WINID_DEV_MOUSE_SENS,
+                  new Rect(0, 0, 400, 60),
+                  MouseSensitivity,
+                  "Mouse Sensitivity");
+    }
+  }
+
+  void MouseSensitivity(int winID)
+  {
+    if (!camControl)
+    {
+      camControl = GameObject.FindObjectOfType(typeof(CameraController)) as CameraController;
+    }
+    GUILayout.BeginHorizontal();
+    GUILayout.Label("Sensitivity");
+    camControl.sensitivity = GUILayout.HorizontalSlider(camControl.sensitivity, 0.01f, 1f);
+    GUILayout.EndHorizontal();
+  }*/
+}
