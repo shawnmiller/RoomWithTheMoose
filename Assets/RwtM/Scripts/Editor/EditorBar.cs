262c9a4842dbf09acacc1e541ff306f03eb23ced
Shawn Miller <shawn.miller0@gmail.com>
2013-10-21 12:25:14 -0400

    editor timeline (laptop sync)

---
+++ Assets/RwtM/Scripts/Editor/EditorBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorBar
+{
+  private Rect drawArea;
+  private Texture2D texture;
+  private BarHandle[] handles;
+
+  public EditorBar(Rect barArea, bool handles, HandleAction action)
+  {
+    drawArea = barArea;
+    if (handles)
+    {
+      this.handles = new BarHandle[2] { new BarHandle(), new BarHandle() };
+    }
+  }
+
+  public void Draw(float clipLeft, float clipRight)
+  {
+
+  }
+}
---
+++ Assets/RwtM/Scripts/Editor/EditorTimeline.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorTimeline
+{
+  private Rect drawArea;
+
+  private float time;
+  private float timescale;
+  private float farRight;
+
+  private float barHeight;
+
+  /// <summary>Creates a new timeline.</summary>
+  /// <param name="screenArea">The area of the window which this timeline will be drawn.</param>
+  /// <param name="time">The full duration of the timeline, regardless of what is shown.</param>
+  /// <param name="startTime">The far-left time value to display.</param>
+  /// <param name="endTime">The far-right time value to display.</param>
+  public EditorTimeline(Rect screenArea, float time, float startTime, float endTime, int barHeight)
+  {
+    drawArea = screenArea;
+    timescale = (endTime - startTime) / time;
+
+  }
+
+  public void ResizeGrid(Rect newSize)
+  {
+    drawArea = newSize;
+  }
+
+  public void ChangeTime(float newTime)
+  {
+    time = newTime;
+  }
+
+  public EditorBar AddBar(int index)
+  {
+    EditorBar bar = new EditorBar(new Rect(), true, HandleAction.ExtentContractBar);
+    return bar;
+  }
+
+  public void Draw()
+  {
+
+  }
+}
