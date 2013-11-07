eb276cd5971e3db0a01aa711e2c5688f8a3616de
Shawn Miller <shawn.miller0@gmail.com>
2013-11-06 21:47:10 -0500

    created TimelineHelper
    a quick script to handle some of the more annoying functionality of the
    timeline

---
+++ Assets/RwtM/Scripts/Editor/TimelineHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class TimelineHelper
+{
+  public static const int SCROLLBAR_SIZE = 30;
+
+  public static int GetOverflowSize(float currentTime, float newTime, float timeScale, Rect timelineArea)
+  {
+    int cPixels = Mathf.FloorToInt(timeScale * timelineArea.width);
+    return Mathf.FloorToInt((cPixels * newTime) / currentTime) + SCROLLBAR_SIZE;
+  }
+
+  public static int ConvertTimeToOverflowPixels(float timeToConvert, int overflowSize, float timelineTime)
+  {
+    return Mathf.FloorToInt(overflowSize * (timeToConvert/timelineTime));
+  }
+}
