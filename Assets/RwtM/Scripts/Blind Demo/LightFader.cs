fcff823ecf321861c76a7ed951dc58bacdb83a36
Shawn Miller <shawn.miller0@gmail.com>
2013-12-11 23:29:30 -0500

    completed demo build

---
+++ Assets/RwtM/Scripts/Blind Demo/LightFader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LightFader : MonoBehaviour
+{
+  public float MinIntensity;
+  public float DegradeSpeed;
+
+  void Update()
+  {
+    light.intensity -= DegradeSpeed * Time.deltaTime;
+    light.intensity = Mathf.Max(light.intensity, MinIntensity);
+  }
+}
---
+++ Assets/RwtM/Scripts/Blind Demo/LightSpawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class LightSpawner : MonoBehaviour
+{
+  public GameObject LightPrefab;
+  public float MinSpawnWaitTime;
+  private float LastSpawnTime;
+  private Transform cam;
+
+  void Start()
+  {
+    cam = Camera.main.transform;
+  }
+
+  void Update()
+  {
+    if(!(Time.time > LastSpawnTime + MinSpawnWaitTime))
+    {
+      return;
+    }
+
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
+      RaycastHit hit;
+
+      if (Physics.Raycast(cam.position, cam.forward, out hit, 5f))
+      {
+        SpawnLight(hit.point - cam.forward*0.5f);
+      }
+    }
+  }
+
+  /*void OnCollisionStay(Collision collision)
+  {
+    if (Time.time > LastSpawnTime + MinSpawnWaitTime)
+    {
+      SpawnLight(collision.contacts[0].point);
+    }
+  }
+  void OnTriggerStay(Collider other)
+  {
+    if (Time.time > LastSpawnTime + MinSpawnWaitTime)
+    {
+      //SpawnLight(other);
+    }
+  }*/
+
+  void SpawnLight(Vector3 position)
+  {
+    Instantiate(LightPrefab, position, Quaternion.identity);
+    LastSpawnTime = Time.time;
+  }
+}
