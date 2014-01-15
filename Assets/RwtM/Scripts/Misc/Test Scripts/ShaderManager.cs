0982843368019898a793a2e4df3378be2beccde5
Shawn Miller <shawn.miller0@gmail.com>
2014-01-15 01:33:36 -0500

    basic shader management

---
+++ Assets/RwtM/Scripts/Misc/Test Scripts/ShaderManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ShaderManager : Singleton<ShaderManager>
+{
+  Transform player;
+
+  void Update()
+  {
+    if (player == null)
+    {
+      player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+  }
+
+  public Vector3 GetPosition()
+  {
+    if (player != null)
+    {
+      return player.position;
+    }
+
+    return Vector3.zero;
+  }
+}
---
+++ Assets/RwtM/Scripts/Misc/Test Scripts/ShaderUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShaderUpdater : MonoBehaviour
+{
+  ShaderManager manager;
+
+  void Start()
+  {
+    manager = ShaderManager.Get();
+  }
+
+  void Update()
+  {
+    Vector4 position = manager.GetPosition();
+    renderer.material.SetVector("_TempTouch", position);
+  }
+}
