defeea899156c77e8c1df4d95a9debda80af6e3d
Shawn Miller <shawn.miller0@gmail.com>
2014-03-19 01:54:41 -0400

    timers

---
+++ Assets/RwtM/Scripts/World/Global Timers/Timer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+  public string Name { get; set; }
+  public float Duration { get; set; }
+  private float ElapsedTime { get; set; }
+  public float RemainingTime { get { return Duration - ElapsedTime; } }
+  public bool IsComplete { get { return ElapsedTime >= Duration; } }
+  public bool IsRunning { get; set; }
+  public bool Obsolete { 
+    get {
+      switch (RepeatMode)
+      {
+        case TimerRepeatType.Count:
+        case TimerRepeatType.Single:
+          return RepeatCount == 0;
+        case TimerRepeatType.Loop:
+          return false;
+        default:
+          return true;
+      }
+    } 
+  }
+  public TimerRepeatType RepeatMode { get; set; }
+  public int RepeatCount { get; set; }
+
+  public Timer()
+  {
+    ElapsedTime = 0;
+  }
+
+  public bool Update(float deltaTime)
+  {
+    if (!IsRunning)
+    {
+      return false;
+    }
+
+    ElapsedTime += deltaTime;
+    bool completed = IsComplete;
+
+    /*if (completed)
+    {
+      RepeatCount -= 1;
+      switch (RepeatMode)
+      {
+        case TimerRepeatType.Single:
+          IsRunning = false;
+          break;
+        case TimerRepeatType.Loop:
+          if (RepeatCount == 0)
+          {
+            IsRunning = false;
+          }
+          break;
+        case TimerRepeatType.Count:
+        default: break;
+      }
+    }*/
+    return completed;
+  }
+}
---
+++ Assets/RwtM/Scripts/World/Global Timers/TimerManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TimerManager : Singleton<TimerManager>
+{
+  private GameState _state;
+  private List<Timer> Timers = new List<Timer>();
+
+  void Start()
+  {
+    _state = GameState.Get();
+  }
+
+  void FixedUpdate()
+  {
+    if (_state.State != StateType.In_Game)
+    {
+      return;
+    }
+
+    bool isComplete;
+    foreach (Timer t in Timers)
+    {
+      if (t.Update(Time.fixedDeltaTime))
+      {
+        ReportCompletedTimer(t);
+        if (t.Obsolete)
+        {
+          Timers.Remove(t);
+        }
+      }
+    }
+  }
+
+  public void CreateTimer(string name, float duration, int repeat, bool autoStart)
+  {
+    Timer newTimer = new Timer();
+    newTimer.Name = name;
+    newTimer.Duration = duration;
+    newTimer.RepeatCount = repeat;
+    newTimer.IsRunning = autoStart;
+    if (repeat == -1)
+    {
+      newTimer.RepeatMode = TimerRepeatType.Loop;
+    }
+    else if (repeat == 1)
+    {
+      newTimer.RepeatMode = TimerRepeatType.Single;
+    }
+    else
+    {
+      newTimer.RepeatMode = TimerRepeatType.Count;
+    }
+
+    Timers.Add(newTimer);
+  }
+
+  public void RemoveTimer(string name)
+  {
+    foreach (Timer t in Timers)
+    {
+      if (t.Name == name)
+      {
+        Timers.Remove(t);
+        break;
+      }
+    }
+  }
+
+  public void StartTimer(string name)
+  {
+    foreach (Timer t in Timers)
+    {
+      if (t.Name == name)
+      {
+        t.IsRunning = true;
+        break;
+      }
+    }
+  }
+
+  void ReportCompletedTimer(Timer timer)
+  {
+    throw new System.NotImplementedException();
+  }
+}
