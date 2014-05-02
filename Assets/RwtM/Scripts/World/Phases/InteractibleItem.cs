2fb052a89d986eebdf819f0d247515e5b163de8c
Shawn Miller <shawn.miller0@gmail.com>
2014-05-02 15:34:14 -0400

    items can now be persistent

--- Assets/RwtM/Scripts/World/Phases/InteractibleItem.cs
+++ Assets/RwtM/Scripts/World/Phases/InteractibleItem.cs
@@ -2,9 +2,19 @@
 
 public class InteractibleItem : MonoBehaviour
 {
+  public bool Persistent = false;
+  public int MessageLimit = 1;
+  private int MessageCount = 0;
+
   public void Use()
   {
-    MessageDispatch.Send(PPS.PP_EVENT_ITEM_PICKUP, this.name);
-    Destroy(gameObject);
+    if (MessageLimit != -1 && MessageCount < MessageLimit)
+    {
+      MessageDispatch.Send(PPS.PP_EVENT_ITEM_PICKUP, this.name);
+      ++MessageCount;
+    }
+
+    if(!Persistent)
+      Destroy(gameObject);
   }
 }
