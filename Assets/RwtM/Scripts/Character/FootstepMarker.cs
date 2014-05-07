29b92f0d7aaba90092406c0ef58d48dbac890425
Shawn Miller <shawn.miller0@gmail.com>
2014-05-07 04:47:24 -0400

    added footstep markers

---
+++ Assets/RwtM/Scripts/Character/FootstepMarker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class FootstepMarker : MonoBehaviour
+{
+  public float FullDuration;
+  private float StartTime;
+
+  public float FadeSpeed;
+  private Vector3 TouchFakePoint;
+
+  private GameObject MyQuad;
+
+  void Start()
+  {
+    StartTime = Time.time;
+    TouchFakePoint = transform.position;
+  }
+
+  void Update()
+  {
+    if (Time.time - StartTime > FullDuration)
+    {
+      Destroy(gameObject);
+    }
+
+    TouchFakePoint += Vector3.up * Time.deltaTime * FadeSpeed;
+    renderer.material.SetVector("_HandPosition", new Vector4(TouchFakePoint.x, TouchFakePoint.y, TouchFakePoint.z, 1));
+  }
+}
---
+++ Assets/RwtM/Scripts/Character/FootstepSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class FootstepSpawner : MonoBehaviour
+{
+  public GameObject FootstepPrefab;
+
+  public float SpawnDistance;
+  public Material MaterialInstance;
+
+  private Vector3 LastStepSpawn;
+
+  void Start()
+  {
+    RaycastHit Hit;
+
+    if (Physics.Raycast(transform.position, Vector3.down, out Hit))
+    {
+      LastStepSpawn = Hit.point;
+    }
+    else
+    {
+      LastStepSpawn = Vector3.zero;
+    }
+  }
+
+  void Update()
+  {
+    RaycastHit Hit;
+
+    if (Physics.Raycast(transform.position, Vector3.down, out Hit))
+    {
+      if (Vector3.Distance(LastStepSpawn, Hit.point) > SpawnDistance)
+      {
+        MakeFootstep(Hit.point);
+      }
+    }
+  }
+
+  private void MakeFootstep(Vector3 Location)
+  {
+    LastStepSpawn = Location;
+    Location.y += 0.05f;
+    Instantiate(FootstepPrefab, Location, Quaternion.Euler(90, 0, 0));
+  }
+}
